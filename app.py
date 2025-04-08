24e3b614ed708020a280cfd5d286addef87a5bbc
Spoortiym <155777510+Spoortiym@users.noreply.github.com>
2025-04-08 16:46:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -400,5 +400,5 @@ def logout():
 
 if __name__ == '__main__':
     init_db()  # Initialize the database
-    app.run(host='0.0.0.0', port=5000)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
